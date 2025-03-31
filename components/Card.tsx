c7874f42f090c2fb2c21f559e2077fd54dddd4c5
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-31 22:59:59 +0200

    Add Card component and integrate with Home page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,8 +1,53 @@
+import { Box, Button, Typography } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 
-const Card = () => {
+interface CardProps {
+    image: string,
+    title: string,
+    description: string,
+    color: string
+}
+
+const Card = ({image, title, description, color}: CardProps) => {
+    
   return (
-    <div>Card</div>
+    <Box sx={{
+        height: '32rem',
+        width: '18.75rem',
+        backgroundColor:color 
+    }}>
+        <Image 
+            src={image}
+            alt={title}
+            height={40}
+            width={64}
+        />
+        <Typography sx={{
+            color: '#ffffff',
+            textTransform: 'uppercase',
+        }}>
+            {title}
+        </Typography>
+        <Typography sx={{
+            color: '#ffffff',
+        }}>
+            {description}
+        </Typography>
+
+        <Button sx={{
+            backgroundColor: color, 
+            color: '#ffffff', 
+            padding: '0.75rem 2rem', 
+            borderRadius: '25px',
+            '&:hover': {
+                backgroundColor: color,
+                opacity: 0.8
+            }
+        }}>
+            Learn More
+        </Button>
+    </Box>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Card from "../../components/Card";
+
 
 export default function Home() {
+  const data = [{
+        image: "/images/icon-sedans.svg", title:"Sedans", description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.", color:'#E28625'
+    },
+    {
+        image: "/images/icon-suvs.svg", title:"suv", description:"Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.", color:'#006971'
+    },
+    {
+        image: "/images/icon-luxury.svg", title:"luxury", description:"Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.", color:'#004140'
+    }]
   return (
+    <div>
+      {data.map((card) => {
+        return <Card image={card.image} title={card.title} description={card.description} color={card.color} key={card.title}/>
+
+      })}
+    </div>
     
   );
 }
