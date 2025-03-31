ff44ea793ed7268c8fa90ebff85dd371395456de
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-31 23:22:26 +0200

    Enhance Card component styling and update Home page layout for better responsiveness

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Typography } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 
+
 interface CardProps {
     image: string,
     title: string,
@@ -11,11 +12,13 @@ interface CardProps {
 
 const Card = ({image, title, description, color}: CardProps) => {
     
+    
   return (
     <Box sx={{
         height: '32rem',
         width: '18.75rem',
-        backgroundColor:color 
+        backgroundColor:color ,
+        padding: '3rem',
     }}>
         <Image 
             src={image}
@@ -26,23 +29,32 @@ const Card = ({image, title, description, color}: CardProps) => {
         <Typography sx={{
             color: '#ffffff',
             textTransform: 'uppercase',
+            fontSize: '2.5rem',
+            fontWeight: 600,
+            margin:"2rem 0"
         }}>
             {title}
         </Typography>
         <Typography sx={{
-            color: '#ffffff',
+            color: 'hsla(0, 0.00%, 100.00%, 0.52)',
+            fontSize: '1.125rem',
+            lineHeight: '2rem',
+            fontWeight: 400,
+            paddingRight: '1rem',
+            marginBottom: '5rem',
         }}>
             {description}
         </Typography>
 
         <Button sx={{
-            backgroundColor: color, 
-            color: '#ffffff', 
+            backgroundColor: 'white', 
+            color: color, 
             padding: '0.75rem 2rem', 
             borderRadius: '25px',
+            border: '3px solid white',
             '&:hover': {
                 backgroundColor: color,
-                opacity: 0.8
+                color: 'white',
             }
         }}>
             Learn More
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+'use client'
+import { Box, useMediaQuery } from "@mui/material";
 import Card from "../../components/Card";
 
 
 export default function Home() {
+
+  const mobile = useMediaQuery('(max-width: 600px)')
+  
   const data = [{
         image: "/images/icon-sedans.svg", title:"Sedans", description:"Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.", color:'#E28625'
     },
@@ -12,12 +17,18 @@ export default function Home() {
         image: "/images/icon-luxury.svg", title:"luxury", description:"Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.", color:'#004140'
     }]
   return (
-    <div>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '98vh',
+      flexDirection: mobile ? 'column' : 'row',
+    }}>
       {data.map((card) => {
         return <Card image={card.image} title={card.title} description={card.description} color={card.color} key={card.title}/>
 
       })}
-    </div>
+    </Box>
     
   );
 }
